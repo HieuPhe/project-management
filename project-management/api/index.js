27d5d9bf4a88e2c14caa8fc924c367117164a6fb
HieuPhe <phehieu2003@gmail.com>
2025-09-02 18:21:50 +0700

    config index, vercel

--- project-management/api/index.js
+++
@@ -1,7 +0,0 @@
-// api/index.js
-const app = require('../index'); // app đã được module.exports ở index.js
-
-// Vercel chỉ cần (req, res)
-module.exports = (req, res) => {
-  return app(req, res);
-};
