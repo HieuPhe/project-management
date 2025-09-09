244b61e10bdbd8fc8c47c8967be585d192efc11d
HieuPhe <phehieu2003@gmail.com>
2025-09-09 16:36:32 +0700

    xây dựng danh sách nhóm quyền

---
+++ project-management/controllers/admin/role.controller.js
@@ -0,0 +1,36 @@
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
+
+//[GET] /admin/role
+
+module.exports.index = async (req, res) => {
+    let find ={
+        deleted: false
+    };
+
+    const records = await Role.find(find);
+
+  res.render("admin/pages/roles/index.pug", {
+    pageTitle: "Nhóm quyền",
+    records: records
+  });
+}
+
+//[GET] /admin/role/create
+
+module.exports.create = async (req, res) => {
+
+  res.render("admin/pages/roles/create.pug", {
+    pageTitle: "Tạo nhóm quyền",
+  });
+}
+
+//[POST] /admin/role/create
+
+module.exports.createPost = async (req, res) => {
+  const record = new Role(req.body);
+  await record.save();
+    
+
+  res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};
