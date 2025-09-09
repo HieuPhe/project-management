144126adc1a6fbb0ad3117335d9490ef5a01b368
HieuPhe <phehieu2003@gmail.com>
2025-09-09 15:34:05 +0700

    thêm danh mục vào thêm mới sản phẩm

--- project-management/controllers/admin/product.controller.js
+++ project-management/controllers/admin/product.controller.js
@@ -5,6 +5,8 @@ const systemConfig = require("../../config/system");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+const createTreeHelper = require("../../helpers/createTree");
+const ProductCategory = require("../../models/product-category.model");
 
 //[GET] /admin/produtcs
 
@@ -141,9 +143,18 @@ module.exports.deleteItem = async (req, res) => {
 // [GET] /admin/products/create
 
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false
+  };
+
+
+  const category = await ProductCategory.find(find);
+
+  const newCategory = createTreeHelper.tree(category);
 
   res.render("admin/pages/products/create", {
     pageTitle: "Thêm mới sản phẩm",
+    category: newCategory
   });
 };
 
