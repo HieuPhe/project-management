08dc7509135fb87fc17b08bfd4dee0f51f148c24
HieuPhe <phehieu2003@gmail.com>
2025-09-09 15:20:02 +0700

    trang sửa danh mục sản phẩm

--- project-management/controllers/admin/product-category.controller.js
+++ project-management/controllers/admin/product-category.controller.js
@@ -9,7 +9,7 @@ module.exports.index = async (req, res) => {
     deleted: false
   };
 
-  
+
 
   const records = await ProductCategory.find(find);
 
@@ -50,7 +50,51 @@ module.exports.createPost = async (req, res) => {
   }
 
   const record = new ProductCategory(req.body);
+
   await record.save();
 
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
+
+
+//[GET] /admin/products-category/edit/:id
+
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false
+    });
+
+    const records = await ProductCategory.find({
+      deleted: false
+    });
+
+    const newRecords = createTreeHelper.tree(records);
+
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+
+
+}
+
+
+//[PATCH] /admin/products-category/edit/:id
+
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne({ _id: id }, req.body);
+
+  res.redirect(req.get('referer') || '/');
+}
