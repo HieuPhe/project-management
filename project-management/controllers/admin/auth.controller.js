e17416fb6781ce5289955ad0a336cfb592ce5aac
HieuPhe <phehieu2003@gmail.com>
2025-09-12 21:35:33 +0700

    middleware cac trang

---
+++ project-management/controllers/admin/auth.controller.js
@@ -0,0 +1,58 @@
+const Account = require("../../models/account.model");
+const systemConfig = require("../../config/system");
+const md5 = require('md5');
+
+
+//[GET] /admin/auth/login
+
+module.exports.login = (req, res) => {
+  if (req.cookies.token) {
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+  } else {
+    res.render("admin/pages/auth/login.pug", {
+      pageTitle: "Đăng nhập"
+    });
+  }
+}
+
+
+//[POST] /admin/auth/login
+
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false
+  })
+
+  if (!user) {
+    req.flash('error', `Email không chính xác!`);
+    res.redirect(req.get('referer') || '/');
+    return;
+  }
+
+  if (md5(password) != user.password) {
+    req.flash('error', `Mật khẩu không chính xác!`);
+    res.redirect(req.get('referer') || '/');
+    return;
+  }
+
+  if (user.status == "inactive") {
+    req.flash('error', `Tài khoản đã bị khóa!`);
+    res.redirect(req.get('referer') || '/');
+    return;
+  }
+  res.cookie("token", user.token);
+  res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+}
+
+
+//[GET] /admin/auth/logout
+
+module.exports.logout = (req, res) => {
+  //Xóa token trong cookies
+  res.clearCookie("token");
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+}
