207ad7dd8cd5289a35da49c40050cd72563e5725
HieuPhe <phehieu2003@gmail.com>
2025-09-13 10:18:49 +0700

    Thêm authorizen

--- project-management/middlewares/admin/auth.middleware.js
+++ project-management/middlewares/admin/auth.middleware.js
@@ -1,19 +1,28 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 const systemConfig = require("../../config/system");
+const { application } = require("express");
 
 
 module.exports.requireAuth = async (req, res, next) => {
 
-    if (!req.cookies.token) {
-        res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+  if (!req.cookies.token) {
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+  } else {
+    console.log(req.cookies.token);
+    const user = await Account.findOne({ token: req.cookies.token }).select("-password");
+    if (!user) {
+      res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
-        console.log(req.cookies.token);
-        const user = await Account.findOne({ token: req.cookies.token });
-        if (!user) {
-            res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
-        } else {
-            next();
-        }
+      const role = await Role.findOne({
+        _id: user.role_id
+      }).select("title permissions");
+
+      res.locals.user = user;
+      res.locals.role = role;
+
+      next();
     }
+  }
 }
