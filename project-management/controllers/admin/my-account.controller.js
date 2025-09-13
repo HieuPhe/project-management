44393645981c905ccaee387504067a3987836f8e
HieuPhe <phehieu2003@gmail.com>
2025-09-13 16:40:34 +0700

    Trang thông tin user bên admin

---
+++ project-management/controllers/admin/my-account.controller.js
@@ -0,0 +1,50 @@
+const Account = require("../../models/account.model");
+const systemConfig = require("../../config/system");
+const md5 = require('md5');
+
+//[GET] /admin/my-account
+
+module.exports.index = async (req, res) => {
+
+    res.render("admin/pages/my-account/index.pug", {
+        pageTitle: "Thông tin cá nhân"
+    });
+}
+
+//[GET] /admin/my-account/edit
+
+module.exports.edit = async (req, res) => {
+
+    res.render("admin/pages/my-account/edit.pug", {
+        pageTitle: "Chỉnh sửa thông tin cá nhân"
+    });
+}
+
+//[PATCH] /admin/my-account/edit
+
+module.exports.editPatch = async (req, res) => {
+    const id = res.locals.user.id;
+
+    const emailExist = await Account.findOne({
+        _id: { $ne: id },
+        email: req.body.email,
+        deleted: false
+    });
+
+    if (emailExist) {
+        req.flash('error', `Email đã tồn tại!`);
+    } else {
+        if (req.body.password) {
+            req.body.password = md5(req.body.password);
+        } else {
+            delete req.body.password;
+        }
+
+        await Account.updateOne({ _id: id }, req.body);
+
+        req.flash('success', `Cập nhật thành công!`);
+    }
+
+    res.redirect(req.get('referer') || '/');
+
+}
--- project-management/middlewares/admin/auth.middleware.js
+++ project-management/middlewares/admin/auth.middleware.js
@@ -10,7 +10,6 @@ module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
   } else {
-    console.log(req.cookies.token);
     const user = await Account.findOne({ token: req.cookies.token }).select("-password");
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
