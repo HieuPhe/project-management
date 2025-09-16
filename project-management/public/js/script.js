4f94c3d50d3877fb403f8d6fc0e17e0f473a5488
HieuPhe <phehieu2003@gmail.com>
2025-09-16 21:41:33 +0700

    thêm sản phẩm vào giỏ hàng

--- project-management/public/js/script.js
+++ project-management/public/js/script.js
@@ -1,1 +1,15 @@
-console.log("OK");
+// Show Alert
+
+const ShowAlert = document.querySelector("[show-alert]");
+if (ShowAlert) {
+
+  const time = parseInt(ShowAlert.getAttribute("data-time")) || 3000;
+  const closeAlert = ShowAlert.querySelector("[close-alert]");
+  setTimeout(() => {
+    ShowAlert.classList.add("alert-hidden");
+  }, time);
+
+  closeAlert.addEventListener("click", () => {
+    ShowAlert.classList.add("alert-hidden");
+  })
+}
