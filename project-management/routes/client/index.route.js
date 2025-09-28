2d2a688b41b0790b2215c3251964340445e32455
HieuPhe <phehieu2003@gmail.com>
2025-09-28 20:43:11 +0700

    hien thi danh sach ket ban

--- project-management/routes/client/index.route.js
+++ project-management/routes/client/index.route.js
@@ -2,7 +2,7 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const userMiddleware = require("../../middlewares/client/user.middleware");
 const settingMiddleware = require("../../middlewares/client/setting.middleware");
-
+const authMiddleware = require("../../middlewares/client/auth.middleware");
 
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
@@ -11,7 +11,8 @@ const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
 const chatRoutes = require("./chat.route");
-const authMiddleware = require("../../middlewares/client/auth.middleware");
+const usersRoutes = require("./users.route");
+
 
 
 
@@ -38,4 +39,6 @@ module.exports = (app) => {
     app.use('/user', userRoutes);
 
     app.use('/chat', authMiddleware.requireAuth, chatRoutes);
+
+    app.use('/users', authMiddleware.requireAuth, usersRoutes);
 }
