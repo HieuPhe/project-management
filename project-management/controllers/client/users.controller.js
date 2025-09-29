99542936b8e94b3d964efeab078d0c748f7af35e
HieuPhe <phehieu2003@gmail.com>
2025-09-29 15:52:29 +0700

    chuc nang huy ket ban

--- project-management/controllers/client/users.controller.js
+++ project-management/controllers/client/users.controller.js
@@ -19,17 +19,45 @@ module.exports.notFriend = async (req, res) => {
 
   const users = await User.find({
     $and: [
-      {_id: { $ne: userId }},
-      {_id: { $nin: requestFriends }},
-      {_id: { $nin: acceptFriends }}
+      { _id: { $ne: userId } },
+      { _id: { $nin: requestFriends } },
+      { _id: { $nin: acceptFriends } }
     ],
 
     status: "active",
     deleted: false
-  }).select("id avatar fullName");  
+  }).select("id avatar fullName");
 
   res.render("client/pages/users/not-friend.pug", {
     pageTitle: "Danh sách người dùng",
+    users: users
+  });
+}
+
+// [GET] /users/request
+
+module.exports.request = async (req, res) => {
+
+  // Socket
+  usersSocket(res);
+
+  const userId = res.locals.user.id;
+
+  const myUser = await User.findOne({
+    _id: userId
+  });
+
+  const requestFriends = myUser.requestFriends;
+
+  const users = await User.find({
+   _id: { $in: requestFriends },
+    status: "active",
+    deleted: false
+  }).select("id avatar fullName");
+
+
+  res.render("client/pages/users/request.pug", {
+    pageTitle: "Lời mời đã gửi",
     users: users
   });
 }
--- project-management/sockets/client/users.socket.js
+++ project-management/sockets/client/users.socket.js
@@ -2,6 +2,7 @@ const User = require("../../models/user.model");
 
 module.exports = (res) => {
   _io.once('connection', (socket) => {
+    // Chức năng gửi yêu cầu
     socket.on("CLIENT_ADD_FRIEND", async (userId) => {
       const myUserId = res.locals.user.id;
 
@@ -37,6 +38,44 @@ module.exports = (res) => {
       }
 
     });
+
+    // Chức năng hủy gửi yêu cầu
+    socket.on("CLIENT_CANCEL_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      // console.log(myUserId); //Id cua A
+      // console.log(userId); //Id cua B
+
+      //Xoa id cua A trong acceptFriend cua B
+      const existIdAinB = await User.findOne({
+        _id: userId,
+        acceptFriends: myUserId
+      });
+
+      if (existIdAinB) {
+        await User.updateOne({
+          _id: userId
+        }, {
+          $pull: { acceptFriends: myUserId }
+        });
+      }
+
+      //Xoa id cua B trong requestFriends cua A
+      const existIdBinA = await User.findOne({
+        _id: myUserId,
+        requestFriends: userId
+      });
+
+      if (existIdBinA) {
+        await User.updateOne({
+          _id: myUserId
+        }, {
+          $pull: { requestFriends: userId }
+        });
+      }
+    });
+
+
   });
 
 }
